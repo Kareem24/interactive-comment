d058712b58de4feddaca9308171a682952eb54d4
kareem roqib <kareemolakunleh@gmail.com>
2023-08-16 03:34:57 +0100

    Feature: implement the edit functionality click

--- js/index.js
+++ js/index.js
@@ -8,14 +8,16 @@ const commentText = getElement(".comment-text");
 const getComments = () => JSON.parse(localStorage.getItem("data"));
 const modal = getElement(".modal");
 const cancelDelete = getElement(".btn-cancel");
-// const el = document.createElement("div");
-// el.classList.add("post-interaction");
+
+const setToStorage = (data) =>
+  localStorage.setItem("data", JSON.stringify(data));
 
 const createElement = (element, ...className) => {
   const el = document.createElement(element);
   className.forEach((singlecClass) => el.classList.add(singlecClass));
   return el;
 };
+
 const allCommentDiv = createElement("div", "post-interaction");
 const createComment = function (users, name = "") {
   const { content, score, user, createdAt, id } = users;
@@ -44,11 +46,12 @@ const createComment = function (users, name = "") {
 							<p class="vote">${score}</p>
 							<button type="button" class="downvote btn"><img src="./images/icon-minus.svg" alt="the minus image for downvote" /></button>
 						</div>
+            <div>
 						<button type="button" class="btn reply-btn  ai-c ${
               user.username === "juliusomo" ? "d-none" : "d-flex"
             }">
 							<img src="./images/icon-reply.svg" alt="the reply icon" />Reply
-						</button>
+						</button></div>
 						<div class="owner-btn ${
               user.username === "juliusomo" ? "d-flex" : "d-none"
             } ai-c">
@@ -67,6 +70,7 @@ const createComment = function (users, name = "") {
 				</article>
 			</div>`;
 };
+
 const loadPage = (data) => {
   const { comments } = data;
   const post = comments.map((person) => createComment(person)).join(" ");
@@ -87,19 +91,21 @@ const loadPage = (data) => {
     const repliedComment = reply[i].replies
       .map(
         (singleReply) =>
-          `<div class="reply-comment d-flex">
-  				<div class="vertical-line"></div>
+          `<div class="reply-comment  d-flex">
   				${createComment(singleReply, reply[i].username)}
+  				<div class="vertical-line"></div>
   		  </div>`,
       )
       .join("");
     singlePost.insertAdjacentHTML("beforeend", repliedComment);
   });
 };
+
 const showModal = () => {
   modal.classList.add("d-flex");
   modal.classList.remove("d-none");
 };
+
 const closeModal = () => {
   modal.classList.add("d-none");
   modal.classList.remove("d-flex");
@@ -110,8 +116,9 @@ const deleteFromStorage = (id) => {
   const { comments } = data;
   const filteredComments = comments.filter((comment) => comment.id !== id);
   data.comments = filteredComments;
-  localStorage.setItem("data", JSON.stringify(data));
+  setToStorage(data);
 };
+
 const deleteReply = (id) => {
   const data = getComments();
   const { comments } = data;
@@ -123,7 +130,15 @@ const deleteReply = (id) => {
       }
     });
   });
-  localStorage.setItem("data", JSON.stringify(data));
+  setToStorage(data);
+};
+
+const confirmDelete = (el) => {
+  el.remove();
+  const { id } = el.dataset;
+  deleteFromStorage(id);
+  deleteReply(parseInt(id, 10));
+  closeModal();
 };
 
 const deleteComment = (e) => {
@@ -136,56 +151,107 @@ const deleteComment = (e) => {
 
     showModal();
     deleteBtn.addEventListener("click", () => {
-      parent.remove();
-      const { id } = parent.dataset;
-      deleteFromStorage(id);
-      deleteReply(parseInt(id, 10));
-      closeModal();
+      confirmDelete(parent);
     });
   }
 };
-const editContent = (e) => {
-  const currTarget = e.target;
+
+const createFormUI = (isEdit) => {
   const form = createElement("form", "comment-form", "bg-white");
 
-  form.innerHTML = `<input
-          type="text"
-          name="comment"
-          placeholder="Add comment"
-          class="comment-text"
-        />
+  form.innerHTML = `<textarea
+					name="comment"
+					id=""
+					cols="30"
+					rows="10"
+					placeholder="Add comment"
+					class="comment-text"></textarea>
         <div class="d-flex ai-c jc-sb">
           <img
             src="./images/avatars/image-juliusomo.webp"
             alt="the account user image"
             class="user-image"
           />
-          <button type="submit" class="submit-comment">Update</button>
+          <button type="submit" class="submit-comment">${
+            isEdit ? "Update" : "Submit"
+          }</button>
         </div>`;
+  return form;
+};
+
+const replyComment = (e) => {
+  const currTarget = e.target;
+  if (currTarget.classList.contains("reply-btn")) {
+    console.log("reply");
+  }
+};
+
+const editComment = (e) => {
+  const currTarget = e.target;
+  const data = getComments();
+  const { comments } = data;
+  const form = createFormUI(true);
+  const comment = form.querySelector(".comment-text");
+  const updateBtn = form.querySelector(".submit-comment");
   if (currTarget.classList.contains("edit-btn")) {
     const parent =
-      currTarget.parentElement.parentElement.parentElement.parentElement;
-    parent.append(form);
+      currTarget.parentElement.parentElement.parentElement.parentElement
+        .parentElement;
+    const paragraph =
+      currTarget.parentElement.parentElement.previousElementSibling;
+    if (parent.classList.contains("reply-comment")) {
+      parent.firstElementChild.append(form);
+      const { id } = parent.firstElementChild.dataset;
+      currTarget.disabled = true;
+      comment.focus();
+      comment.value = paragraph.textContent;
 
-    currTarget.disabled = true;
+      updateBtn.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        if (comment.value === "") return;
+
+        comments.forEach(({ replies }) => {
+          replies.forEach((reply) => {
+            if (reply.id === +id) {
+              // eslint-disable-next-line no-param-reassign
+              reply.content = comment.value;
+              paragraph.textContent = comment.value;
+
+              setToStorage(data);
+              form.remove();
+              currTarget.disabled = false;
+            }
+          });
+        });
+      });
+
+      // commentText.focus();
+    }
+    if (parent.classList.contains("post-interaction")) {
+      parent.append(form);
+      const { id } =
+        currTarget.parentElement.parentElement.parentElement.parentElement
+          .dataset;
+
+      currTarget.disabled = true;
+      comment.focus();
+      comment.value = paragraph.textContent;
+      updateBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        // change the text content of the paragph
+        paragraph.textContent = comment.value;
+        // change back to submit
+        updateBtn.textContent = "Submit";
+        currTarget.disabled = false;
+
+        // edit from local storage
+        comments.find((comt) => comt.id === id).content = comment.value;
+        comment.value = "";
+        setToStorage(data);
+        form.remove();
+      });
+    }
   }
-  const updateBtn = form.querySelector(".submit-comment");
-  updateBtn.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    currTarget.disabled = false;
-    form.remove();
-  });
-};
-// select the edit button
-// get the parent element of the edit button
-// insert form after the parent elment
-// get the current tex as the form value
-// change the send button to update
-const addCommentUI = (comment) => {
-  const commentDiv = createElement("div", "active-div");
-  commentDiv.setAttribute("data-id", comment.id);
-  commentDiv.innerHTML = createComment(comment);
-  allCommentDiv.append(commentDiv);
 };
 
 const addComment = (e) => {
@@ -196,17 +262,25 @@ const addComment = (e) => {
   const id = new Date().getTime().toString();
   const { currentUser } = data;
   const content = value;
+  const date = new Date();
+  const locale = navigator.language;
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "numeric",
+  };
+  const globalDate = new Intl.DateTimeFormat(locale, options).format(date);
   const newComment = {
     id,
-    // eslint-disable-next-line new-cap
-    createdAt: new Date().getUTCDay(),
+    createdAt: globalDate,
     score: 0,
     replies: [],
     content,
     user: currentUser,
   };
   data.comments.push(newComment);
-  addCommentUI(newComment);
+  allCommentDiv.insertAdjacentHTML("beforeend", createComment(newComment));
+
   localStorage.setItem("data", JSON.stringify(data));
   commentText.value = "";
 };
@@ -232,6 +306,7 @@ window.addEventListener("DOMContentLoaded", getData);
 submitBtn.addEventListener("click", addComment);
 commentContainer.addEventListener("click", (e) => {
   deleteComment(e);
-  editContent(e);
+  editComment(e);
+  replyComment(e);
 });
 cancelDelete.addEventListener("click", closeModal);
